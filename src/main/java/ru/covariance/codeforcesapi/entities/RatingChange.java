4062b1c742c329fe5dbd7e8e62b9561bb4ef381e
CovarianceMomentum <covariancemomentum@gmail.com>
2021-02-10 21:18:32 +0300

    Fixed codestyle according to JSL in RatingChange.java

--- src/main/java/ru/covariance/codeforcesapi/entities/RatingChange.java
+++ src/main/java/ru/covariance/codeforcesapi/entities/RatingChange.java
@@ -5,11 +5,40 @@ import lombok.Data;
 @Data
 public class RatingChange {
 
+  /**
+   * Contest id.
+   */
   private int contestId;
-  private String contestName; // Localized.
-  private String handle; // Codeforces user handle.
-  private int rank; // Place of the user in the contest. This field contains user rank on the moment of rating update. If afterwards rank changes (e.g. someone get disqualified), this field will not be update and will contain old rank.
-  private int ratingUpdateTimeSeconds; // Time, when rating for the contest was update, in unix-format.
-  private int oldRating; // User rating before the contest.
-  private int newRating; // User rating after the contest.
+
+  /**
+   * Localized.
+   */
+  private String contestName;
+
+  /**
+   * Codeforces user handle.
+   */
+  private String handle;
+
+  /**
+   * Place of the user in the contest. This field contains user rank on the moment of rating update.
+   * If afterwards rank changes (e.g. someone get disqualified), this field will not be update and
+   * will contain old rank.
+   */
+  private int rank;
+
+  /**
+   * Time, when rating for the contest was update, in unix-format.
+   */
+  private int ratingUpdateTimeSeconds;
+
+  /**
+   * User rating before the contest.
+   */
+  private int oldRating;
+
+  /**
+   * User rating after the contest.
+   */
+  private int newRating;
 }
