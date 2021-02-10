6ad8a755bc1eb80a9b5d8b83cbfeb56b5fc8bdf1
CovarianceMomentum <covariancemomentum@gmail.com>
2021-02-10 21:14:14 +0300

    Fixed codestyle according to JSL in ProblemStatistics.java

--- src/main/java/ru/covariance/codeforcesapi/entities/ProblemStatistics.java
+++ src/main/java/ru/covariance/codeforcesapi/entities/ProblemStatistics.java
@@ -5,7 +5,19 @@ import lombok.Data;
 @Data
 public class ProblemStatistics {
 
-  private int contestId; // Can be absent. Id of the contest, containing the problem.
-  private String index; // Usually a letter of a letter, followed by a digit, that represent a problem index in a contest.
-  private int solvedCount; // Number of users, who solved the problem.
+  /**
+   * Can be absent. Id of the contest, containing the problem.
+   */
+  private int contestId;
+
+  /**
+   * Usually a letter of a letter, followed by a digit, that represent a problem index in a
+   * contest.
+   */
+  private String index;
+
+  /**
+   * Number of users, who solved the problem.
+   */
+  private int solvedCount;
 }
