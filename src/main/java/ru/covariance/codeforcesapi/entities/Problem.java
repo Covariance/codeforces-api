6eaedb140fcc9a99bb660917fed7dbb85f529b7c
CovarianceMomentum <covariancemomentum@gmail.com>
2021-02-10 21:07:22 +0300

    Fixed codestyle according to JSL in Problem.java

--- src/main/java/ru/covariance/codeforcesapi/entities/Problem.java
+++ src/main/java/ru/covariance/codeforcesapi/entities/Problem.java
@@ -5,12 +5,44 @@ import lombok.Data;
 
 @Data
 public class Problem {
-  private int contestId; // Can be absent. Id of the contest, containing the problem.
-  private String problemsetName; // Can be absent. Short name of the problemset the problem belongs to.
-  private String index; // Usually a letter of a letter, followed by a digit, that represent a problem index in a contest.
-  private String name; // Localized.
-  private String type; //	Enum: PROGRAMMING, QUESTION.
-  private double points; //	Floating point number. Can be absent. Maximum amount of points for the problem.
-  private int rating; // Can be absent. Problem rating (difficulty).
-  private List<String> tags; // Problem tags.
+
+  /**
+   * Can be absent. Id of the contest, containing the problem.
+   */
+  private int contestId;
+
+  /**
+   * Can be absent. Short name of the problemset the problem belongs to.
+   */
+  private String problemsetName;
+
+  /**
+   * Usually a letter followed by a digit that represent a problem index in a contest.
+   */
+  private String index;
+
+  /**
+   * Localized.
+   */
+  private String name;
+
+  /**
+   * Enum: PROGRAMMING, QUESTION.
+   */
+  private String type;
+
+  /**
+   * Floating point number. Can be absent. Maximum amount of points for the problem.
+   */
+  private double points;
+
+  /**
+   * Can be absent. Problem rating (difficulty).
+   */
+  private int rating;
+
+  /**
+   * Problem tags.
+   */
+  private List<String> tags;
 }
