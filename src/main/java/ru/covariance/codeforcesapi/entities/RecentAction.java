fe5b0722f4c7ea48bd861b1d30f27c36b1ea310e
CovarianceMomentum <covariancemomentum@gmail.com>
2021-02-10 20:56:10 +0300

    Fixed codestyle according to JSL in RecentAction.java

--- src/main/java/ru/covariance/codeforcesapi/entities/RecentAction.java
+++ src/main/java/ru/covariance/codeforcesapi/entities/RecentAction.java
@@ -6,6 +6,6 @@ import lombok.Data;
 public class RecentAction {
 
   private int timeSeconds; // Action time, in unix format.
-  private BlogEntry blogEntry; //	BlogEntry object in short form. Can be absent.
-  private Comment comment; //	Comment object. Can be absent.
+  private BlogEntry blogEntry; // BlogEntry object in short form. Can be absent.
+  private Comment comment; // Comment object. Can be absent.
 }
