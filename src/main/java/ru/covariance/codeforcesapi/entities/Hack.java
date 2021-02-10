a7d29d3ec99327f4b364ce62a8dfadab071a00f7
CovarianceMomentum <covariancemomentum@gmail.com>
2021-02-10 21:25:34 +0300

    Fixed codestyle according to JSL in Hack.java

--- src/main/java/ru/covariance/codeforcesapi/entities/Hack.java
+++ src/main/java/ru/covariance/codeforcesapi/entities/Hack.java
@@ -6,25 +6,57 @@ import lombok.Data;
 public class Hack {
 
   /**
-   * Object with three fields: "manual", "protocol" and "verdict".
-   * Field manual can have values "true" and "false".
-   * If manual is "true" then test for the hack was entered manually.
-   * Fields "protocol" and "verdict" contain human-readable
-   * description of judge protocol and hack verdict.
+   * Object with three fields: "manual", "protocol" and "verdict". Field manual can have values
+   * "true" and "false". If manual is "true" then test for the hack was entered manually. Fields
+   * "protocol" and "verdict" contain human-readable description of judge protocol and hack
+   * verdict.
    */
   @Data
   static class JudgeProtocol {
+
     private boolean manual;
     private String protocol;
     private String verdict;
   }
 
+  /**
+   * Hack id.
+   */
   private int id;
-  private int creationTimeSeconds; // Hack creation time in unix format.
-  private Party hacker; // Party object.
-  private Party defender; // Party object.
-  private String verdict; // Enum: HACK_SUCCESSFUL, HACK_UNSUCCESSFUL, INVALID_INPUT, GENERATOR_INCOMPILABLE, GENERATOR_CRASHED, IGNORED, TESTING, OTHER. Can be absent.
-  private Problem problem; // Problem object. Hacked problem.
-  private String test; // Can be absent.
-  private JudgeProtocol judgeProtocol; // Localized. Can be absent.
+
+  /**
+   * Hack creation time in unix format.
+   */
+  private int creationTimeSeconds;
+
+  /**
+   * Party object.
+   */
+  private Party hacker;
+
+  /**
+   * Party object.
+   */
+  private Party defender;
+
+  /**
+   * Enum: HACK_SUCCESSFUL, HACK_UNSUCCESSFUL, INVALID_INPUT, GENERATOR_INCOMPILABLE,
+   * GENERATOR_CRASHED, IGNORED, TESTING, OTHER. Can be absent.
+   */
+  private String verdict;
+
+  /**
+   * Problem object. Hacked problem.
+   */
+  private Problem problem;
+
+  /**
+   * Can be absent.
+   */
+  private String test;
+
+  /**
+   * Localized. Can be absent.
+   */
+  private JudgeProtocol judgeProtocol;
 }
