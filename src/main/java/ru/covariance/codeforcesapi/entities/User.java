fc3f44d4b8d10732cfb4a5ce3f4e7fa9ab53b242
CovarianceMomentum <covariancemomentum@gmail.com>
2021-02-10 21:17:09 +0300

    Fixed codestyle according to JSL in User.java

--- src/main/java/ru/covariance/codeforcesapi/entities/User.java
+++ src/main/java/ru/covariance/codeforcesapi/entities/User.java
@@ -5,23 +5,98 @@ import lombok.Data;
 @Data
 public class User {
 
-  private String handle; // Codeforces user handle.
-  private String email; // Shown only if user allowed to share his contact info.
-  private String vkId;  // User id for VK social network. Shown only if user allowed to share his contact info.
-  private String openId; // Shown only if user allowed to share his contact info.
-  private String firstName;  // Localized. Can be absent.
-  private String lastName;  // Localized. Can be absent.
-  private String country;  // Localized. Can be absent.
-  private String city;  // Localized. Can be absent.
-  private String organization;  // Localized. Can be absent.
-  private int contribution;  // User contribution.
-  private String rank;  // Localized. Current user rank.
-  private int rating;  // Current user rating.
-  private String maxRank;  // Localized. Max user rank.
-  private int maxRating; // Max user rating.
-  private int lastOnlineTimeSeconds; // Time, when user was last seen online, in unix format.
-  private int registrationTimeSeconds; // Time, when user was registered, in unix format.
-  private int friendOfCount; // Amount of users who have this user in friends.
-  private String avatar; // User's avatar URL.
-  private String titlePhoto;  // User's title photo URL.
+  /**
+   * Codeforces user handle.
+   */
+  private String handle;
+
+  /**
+   * Shown only if user allowed to share his contact info.
+   */
+  private String email;
+
+  /**
+   * User id for VK social network. Shown only if user allowed to share his contact info.
+   */
+  private String vkId;
+
+  /**
+   * Shown only if user allowed to share his contact info.
+   */
+  private String openId;
+
+  /**
+   * Localized. Can be absent.
+   */
+  private String firstName;
+
+  /**
+   * Localized. Can be absent.
+   */
+  private String lastName;
+
+  /**
+   * Localized. Can be absent.
+   */
+  private String country;
+
+  /**
+   * Localized. Can be absent.
+   */
+  private String city;
+
+  /**
+   * Localized. Can be absent.
+   */
+  private String organization;
+
+  /**
+   * User contribution.
+   */
+  private int contribution;
+
+  /**
+   * Localized. Current user rank.
+   */
+  private String rank;
+
+  /**
+   * Current user rating.
+   */
+  private int rating;
+
+  /**
+   * Localized. Max user rank.
+   */
+  private String maxRank;
+
+  /**
+   * Max user rating.
+   */
+  private int maxRating;
+
+  /**
+   * Time, when user was last seen online, in unix format.
+   */
+  private int lastOnlineTimeSeconds;
+
+  /**
+   * Time, when user was registered, in unix format.
+   */
+  private int registrationTimeSeconds;
+
+  /**
+   * Amount of users who have this user in friends.
+   */
+  private int friendOfCount;
+
+  /**
+   * User's avatar URL.
+   */
+  private String avatar;
+
+  /**
+   * User's title photo URL.
+   */
+  private String titlePhoto;
 }
